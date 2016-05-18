bd9ae8c7923e258c647a6a0d962bc9224eebccf3
Eric Ross <eric.ross@hp.com>
2016-05-18 16:06:01 -0700

    playing around with optional field

--- python/address.py
+++ python/address.py
@@ -2,13 +2,16 @@ import addressbook_pb2
 address_book = addressbook_pb2.AddressBook()
 person = address_book.person.add()
 
-person.id = 1234
+person.id = 12L
 person.name = "John Doe"
 person.email = "jdoe@example.com"
 phone = person.phone.add()
 phone.number = "555-4321"
 phone.type = addressbook_pb2.Person.HOME
 
+person = address_book.person.add()
+person.id=52
+person.name = "somebody else"
 
 
 f=open("testit","wb")
@@ -19,7 +22,8 @@ f=open("testit","rb")
 address_book = addressbook_pb2.AddressBook()
 address_book.ParseFromString(f.read())
 for person in address_book.person:
-    print person.name, person.id
+    print person.name, person.id, person.email
+    print "email is there: ", person.HasField("email")
 f.close()
 
 print type(address_book)
