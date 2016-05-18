c02ff936a26735a85aa4d47f6be4c61123cd9d16
Eric Ross <eric.ross@hp.com>
2016-05-18 16:06:34 -0700

    create beginnings of clien/server

---
+++ python/address_server.py
@@ -0,0 +1,40 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello():
+    return "Hello world"
+
+import addressbook_pb2
+address_book = addressbook_pb2.AddressBook()
+person = address_book.person.add()
+
+person.id = 12L
+person.name = "John Doe"
+person.email = "jdoe@example.com"
+phone = person.phone.add()
+phone.number = "555-4321"
+phone.type = addressbook_pb2.Person.HOME
+
+person = address_book.person.add()
+person.id=52
+person.name = "somebody else"
+
+
+f=open("testit","wb")
+f.write(address_book.SerializeToString())
+f.close()
+
+f=open("testit","rb")
+address_book = addressbook_pb2.AddressBook()
+address_book.ParseFromString(f.read())
+for person in address_book.person:
+    print person.name, person.id, person.email
+    print "email is there: ", person.HasField("email")
+f.close()
+
+print type(address_book)
+
+if __name__ == "__main__":
+    app.run()
